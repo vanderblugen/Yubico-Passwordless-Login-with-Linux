e12589ceb2488e6f91c3f4b1a45165d49622b75a
vanderblugen <33649889+vanderblugen@users.noreply.github.com>
2023-01-09 06:52:33 -0600

    Delete finalize.sh
    Not needed

--- finalize.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://raw.githubusercontent.com/vanderblugen/Yubico-Passwordless-Login-with-Linux/master/update-mapping.sh
-chmod +x updadate-mapping.sh
-sudo ./update-mapping.sh
